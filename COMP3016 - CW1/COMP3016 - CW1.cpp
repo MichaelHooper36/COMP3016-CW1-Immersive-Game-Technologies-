ad33fc64d642eb8207bd037f55c892860dc5f0c7
Michael Hooper <mikeyhooper360@gmail.com>
2025-10-27 17:20:00 +0000

    Window finished

--- COMP3016 - CW1/COMP3016 - CW1.cpp
+++ COMP3016 - CW1/COMP3016 - CW1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 bool gameOver;
-const int width = 20;
+const int width = 60;
 const int height = 20;
 int x, y;
 enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
@@ -14,10 +14,35 @@ Direction dir;
 void Setup() {
 	gameOver = false;
 	dir = STOP;
+	x = width / 2;
+	y = height / 2;
 }
 
 void Draw() {
 	system("cls");
+	// Top border
+	for(int i=0;i<width+2; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (j == 0)
+				cout << "#"; // Left border
+			if (i == y && j == x)
+				cout << "O"; // Player position
+			else
+				cout << " "; // Empty space
+			if (j == width - 1)
+				cout << "#"; // Right border
+		}
+		cout << endl;
+	}
+
+	// Bottom border
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+	cout << endl;
 }
 
 void Input() {
@@ -42,14 +67,51 @@ void Input() {
 	}
 }
 
-void Logic() {}
+void Logic() {
+	switch (dir) {
+		case LEFT:
+			if (x > 0)
+			{
+				x--;
+				dir = STOP; // Stop movement after one step
+				Draw();
+			}
+			break;
+		case RIGHT:
+			if (x < width - 1)
+			{
+				x++;
+				dir = STOP; // Stop movement after one step
+				Draw();
+			}
+			break;
+		case UP:
+			if (y > 0)
+			{
+				y--;
+				dir = STOP; // Stop movement after one step
+				Draw();
+			}
+			break;
+		case DOWN:
+			if (y < height - 1)
+			{
+				y++;
+				dir = STOP; // Stop movement after one step
+				Draw();
+			}
+			break;
+		default:
+			break;
+	}
+}
 
 int main() {
 	Setup();
+	Draw();
 	while (!gameOver) {
-		Draw();
 		Input();
 		Logic();
-		Sleep(100);
+		Sleep(300);
 	}
 }
